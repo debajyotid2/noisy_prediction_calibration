Fix bug of wrong calculation of alpha prior in NPC training

main.py
@@ -53,9 +53,6 @@ def main(args: Args):
     train_ds = make_dataset(x_train, y_train, args.dataset.batch_size)
     val_ds = make_dataset(x_val, y_val, args.dataset.batch_size)
 
-    # Tensorboard callback
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=args.training.log_dir)
-
     # Initialize classifier and autoencoder
     classifier = CNN(
         n_classes=args.dataset.n_classes,
@@ -92,7 +89,7 @@ def main(args: Args):
     gc.collect()
 
     # Create dataset for prior generation
-    train_pred_ds = make_dataset(x_train, y_pred)
+    train_pred_ds = make_dataset(x_train, y_pred, args.dataset.batch_size)
 
     # Generate prior
     prior_labels, prior_probabilities = generate_prior(

src/prior.py
@@ -54,14 +54,20 @@ def generate_prior(
             embeddings.append(embedding[class_idxs[gathered[-1]]])
             classes.append(class_id)
 
+    logging.info(f"Initial embeddings generated in {(perf_counter() - start_time)/60:.2f} minutes.")
+
     # Fit classifier embeddings to classes in KNN
+
+    start_time = perf_counter()
     classes = np.asarray(classes, dtype=np.int32)
     embeddings = np.asarray(embeddings, dtype=np.int32)
     knn.fit(embeddings, classes)
     del embeddings
 
+    logging.info(f"KNN trained in {(perf_counter() - start_time)/60:.2f} minutes.")
     # Generate predictions using KNN for classes
     # NOTE: New embeddings must be generated for prediction
+    start_time = perf_counter()
     embeddings = np.zeros((len_train, int(classifier.dim**2.0)))
     for idx, (x_batch, _) in enumerate(dataset):
         img_emb, _ = classifier(x_batch)
@@ -69,8 +75,11 @@ def generate_prior(
             idx * x_batch.shape[0] : (idx + 1) * x_batch.shape[0]
         ] = img_emb.numpy()
 
-    output_labels = knn.predict(embeddings)
+    logging.info(f"New embeddings generated in {(perf_counter() - start_time)/60:.2f} minutes.")
+    
+    start_time = perf_counter()
     output_probs = knn.predict_proba(embeddings)
+    output_labels = np.argmax(output_probs, axis=-1)
     padded = np.zeros((output_probs.shape[0], n_classes))
     padded[:, knn.classes_] = output_probs
 

src/training.py
@@ -39,7 +39,7 @@ class ClassifierTrainStep:
         self.model = model
         self.optimizer = optimizer
         self.loss_func = loss_func
-    
+
     @tf.function
     def __call__(
         self, x_batch: tf.Tensor, y_batch: tf.Tensor
@@ -52,7 +52,7 @@ class ClassifierTrainStep:
             loss = self.loss_func(y_batch, logits)
         grad = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(grad, self.model.trainable_variables))
-        
+
         y_pred = tf.argmax(tf.nn.softmax(logits), axis=-1)
         y_batch = tf.argmax(y_batch, axis=-1)
         correct = tf.where(y_pred == y_batch, 1.0, 0.0)
@@ -99,7 +99,7 @@ class AETrainStep:
         self.ae = autoencoder
         self.optimizer = optimizer
         self.bce_with_logits = tf.keras.losses.BinaryCrossentropy(
-            from_logits=True, reduction=tf.keras.losses.Reduction.SUM
+            from_logits=True, reduction=tf.keras.losses.Reduction.NONE
         )
         self.kld_reg = kld_reg
         self.prior_norm = prior_norm
@@ -107,19 +107,21 @@ class AETrainStep:
     @tf.function
     def __call__(
         self, x_batch: tf.Tensor, y_pred: tf.Tensor, y_prior: tf.Tensor
-    ) -> tf.Tensor:
+    ) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
         """
         Training step.
         """
         with tf.GradientTape() as tape:
             y_gen, encoded_ypred = self.ae(x_batch, y_pred)
-            rec_loss = self.bce_with_logits(y_pred, y_gen)
-            alpha_prior = self.prior_norm * y_prior
-            dist_loss = kl_divergence(alpha_prior, encoded_ypred)
-            loss = tf.reduce_mean(rec_loss + self.kld_reg * dist_loss)
+            rec_loss = tf.reduce_mean(self.bce_with_logits(y_pred, y_gen))
+            alpha_prior = self.prior_norm * y_prior + 1.0 + 1.0 / y_gen.shape[-1]
+            dist_loss = self.kld_reg * tf.reduce_mean(
+                kl_divergence(alpha_prior, encoded_ypred)
+            )
+            loss = rec_loss + dist_loss
         grad = tape.gradient(loss, self.ae.trainable_variables)
         self.optimizer.apply_gradients(zip(grad, self.ae.trainable_variables))
-        return loss
+        return loss, rec_loss, dist_loss
 
 
 def train_classifier(
@@ -208,15 +210,29 @@ def train_npc(
     train_step = AETrainStep(autoencoder, optimizer, kld_reg, prior_norm)
     for epoch in range(num_epochs):
         train_loss = 0.0
+        rec_loss_train = 0.0
+        dist_loss_train = 0.0
+        rec_losses = []
+        dist_losses = []
         for x_batch, y_pred, y_prior in train_npc_ds:
             # Generate alpha prior
-            loss = train_step(x_batch, y_pred, y_prior)
+            loss, rec_loss, dist_loss = train_step(x_batch, y_pred, y_prior)
             train_loss += loss
+            rec_loss_train += rec_loss
+            dist_loss_train += dist_loss
+            rec_losses.append(rec_loss)
+            dist_losses.append(dist_loss)
+        breakpoint()
 
         # Log metrics
         with train_summary_writer.as_default():
             tf.summary.scalar("train_loss", train_loss, step=epoch)
+            tf.summary.scalar("rec_loss", rec_loss_train, step=epoch)
+            tf.summary.scalar("dist_loss", dist_loss_train, step=epoch)
 
         logging.info(
-            f"Epoch [{epoch+1}/{num_epochs}]," f" train loss: {train_loss:.4f},"
+            f"Epoch [{epoch+1}/{num_epochs}],"
+            f" train loss: {train_loss:.4f},"
+            f" reconstruction loss: {rec_loss_train:.4f},"
+            f" distribution loss: {dist_loss_train:.4f},"
         )